fd2d39ee04c473524bedd909163af3249256c9c0
aljosa <aljosa.mrak@gmail.com>
2014-07-24 01:05:35 +0200

    initial commit with changed README.md

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Arduino-SPI
 ===========
 
 Arduino SPI library changed so it works on Arduino and ATtiny44, 84, 85, 45.
+
+This libray can be used for Arduino projects and ATtiny project without changing code.
---
+++ SPI.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2010 by Cristian Maglie <c.maglie@bug.st>
+ * SPI Master library for arduino.
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of either the GNU General Public License version 2
+ * or the GNU Lesser General Public License version 2.1, both as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _SPI_H_INCLUDED
+#define _SPI_H_INCLUDED
+
+#include <stdio.h>
+#include <Arduino.h>
+#include <avr/pgmspace.h>
+
+/*
+ * Pin definitions for ATtiny44, ATtiny84, ATtiny45, ATtiny85 and Arduino. Now onli numbers work. 
+ */
+#if defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  #define SPI_DDR_PORT 3
+  #define USCK_DD_PIN  6
+  #define DO_DD_PIN    5
+  #define DI_DD_PIN    4
+  #define __ATTINY
+#elif defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  #define SPI_DDR_PORT 1
+  #define USCK_DD_PIN  4
+  #define DO_DD_PIN    2
+  #define DI_DD_PIN    3
+  #define __ATTINY
+#else
+  #define SPI_DDR_PORT SS
+  #define USCK_DD_PIN  SCK
+  #define DO_DD_PIN    MOSI
+  #define DI_DD_PIN    MISO
+#endif
+
+
+#define SPI_CLOCK_DIV4   0x00
+#define SPI_CLOCK_DIV16  0x01
+#define SPI_CLOCK_DIV64  0x02
+#define SPI_CLOCK_DIV128 0x03
+#define SPI_CLOCK_DIV2   0x04
+#define SPI_CLOCK_DIV8   0x05
+#define SPI_CLOCK_DIV32  0x06
+//#define SPI_CLOCK_DIV64 0x07
+
+#define SPI_MODE0 0x00
+#define SPI_MODE1 0x04
+#define SPI_MODE2 0x08
+#define SPI_MODE3 0x0C
+
+#define SPI_MODE_MASK    0x0C  // CPOL = bit 3, CPHA = bit 2 on SPCR
+#define SPI_CLOCK_MASK   0x03  // SPR1 = bit 1, SPR0 = bit 0 on SPCR
+#define SPI_2XCLOCK_MASK 0x01  // SPI2X = bit 0 on SPSR
+
+class SPIClass {
+public:
+  inline static byte transfer(byte _data);
+  
+  // SPI Configuration methods
+
+  #ifndef __ATTINY							// Defined only if not ATtiny 
+    inline static void attachInterrupt();
+    inline static void detachInterrupt(); 	// Default
+    static void setBitOrder(uint8_t);
+  #endif
+
+  static void begin(); // Default
+  static void end();
+    
+  static void setDataMode(uint8_t);
+  static void setClockDivider(uint8_t);
+};
+
+extern SPIClass SPI;
+
+uint8_t SPIClass::transfer(uint8_t _data) {
+
+  #ifdef __ATTINY
+    USIDR = _data;
+    USISR = _BV(USIOIF);
+  
+    while((USISR & _BV(USIOIF)) == 0) {
+      USICR = _BV(USIWM0) | _BV(USICS1) | _BV(USICLK) | _BV(USITC);
+    }
+    return USIDR;
+  #else
+    SPDR = _data;
+    while (!(SPSR & _BV(SPIF)))
+      ;
+    return SPDR;
+  #endif
+}
+
+#ifndef __ATTINY
+  void SPIClass::attachInterrupt() {
+    SPCR |= _BV(SPIE);
+  }
+
+  void SPIClass::detachInterrupt() {
+    SPCR &= ~_BV(SPIE);
+  }
+#endif
+
+#endif
